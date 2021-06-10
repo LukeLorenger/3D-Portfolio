10a9da2431f6da7245911cc5302bcc14bdbf00b4
Luke Lorenger <dirtydeadtv@gmail.com>
2021-06-09 23:04:17 -0400

    LightHelper and Orbital Controls added.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 // scene
 const scene = new THREE.Scene();
 
@@ -21,10 +23,25 @@ renderer.render( scene, camera );
 
 // Geometry - Add an object
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100 )
-const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus)
+
+// Light
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20,20,20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+// Light Helper
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+
+// Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // Loop
 function animate(){
@@ -33,6 +50,8 @@ function animate(){
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
 
   renderer.render( scene, camera );
 }
