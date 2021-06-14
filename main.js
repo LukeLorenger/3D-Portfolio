bdc672e3825b3c231e9a9fd819087bdd81c2ef47
Luke Lorenger <dirtydeadtv@gmail.com>
2021-06-14 10:27:07 -0400

    other planets and deathstar added

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { FlyControls } from 'three/examples/jsm/controls/FlyControls';
+
 
 // scene
+
 const scene = new THREE.Scene();
 
 // camera
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 // renderer
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 })
@@ -20,6 +25,7 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 // Geometry - Add an object
+
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100 )
 const donutTexture = new THREE.TextureLoader().load('normal.jpg');
 // new THREE.MeshBasicMaterial( { map: lukeTexture } )
@@ -29,7 +35,11 @@ const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus)
 
+//controls
+
+
 // Light
+
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(20,20,20)
 
@@ -37,14 +47,17 @@ const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
 
 // Light Helper
+
 const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper)
 
 // Orbit Controls
+
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // Stars
+
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial( { color: 0xffffff } )
@@ -60,13 +73,13 @@ Array(200).fill().forEach(addStar)
 
 
 // Space texture
+
 const spaceTexture = new THREE.TextureLoader().load('space.jpeg');
 scene.background = spaceTexture;
 
 // Avatar
-const lukeTexture = new THREE.TextureLoader().load('me.jpg');
-const planetATexture = new THREE.TextureLoader().load('planetA.jpg');
 
+const lukeTexture = new THREE.TextureLoader().load('me.jpg');
 const luke = new THREE.Mesh(
   new THREE.BoxGeometry(3,3,3),
   // new THREE.CylinderGeometry( 5, 5, 1, 32 ),
@@ -76,6 +89,8 @@ const luke = new THREE.Mesh(
 scene.add(luke);
 
 // Planet A
+
+const planetATexture = new THREE.TextureLoader().load('planetA.jpg');
 const planetA = new THREE.Mesh(
   new THREE.DodecahedronGeometry(3,3,3),
   new THREE.MeshBasicMaterial( { map: planetATexture } )
@@ -83,7 +98,28 @@ const planetA = new THREE.Mesh(
 
 scene.add(planetA)
 
+// Planet B
+
+const planetBTexture = new THREE.TextureLoader().load('planetB.jpg');
+const planetB = new THREE.Mesh(
+  new THREE.DodecahedronGeometry(30,30,30),
+  new THREE.MeshBasicMaterial( { map: planetBTexture } )
+);
+
+scene.add(planetB)
+
+// Planet C
+
+const planetCTexture = new THREE.TextureLoader().load('deathstar.png');
+const planetC = new THREE.Mesh(
+  new THREE.DodecahedronGeometry(10,10,10),
+  new THREE.MeshBasicMaterial( { map: planetCTexture } )
+);
+
+scene.add(planetC)
+
 // Moon
+
 const moonTexture = new THREE.TextureLoader().load('moon.jpg');
 const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 
@@ -95,51 +131,50 @@ const moon = new THREE.Mesh(
   })
 );
 
-// Moon2
-// const moon2Texture = new THREE.TextureLoader().load('moon.jpg');
-// const norma2lTexture = new THREE.TextureLoader().load('normal.jpg');
-
-// const moon2 = new THREE.Mesh(
-//   new THREE.SphereGeometry(3, 32, 32),
-//   new THREE.MeshStandardMaterial({
-//     map: moon2Texture,
-//     normalMap: normal2Texture,
-//   })
-// );
-
 scene.add(moon)
-// scene.add(moon2)
 
-// Positioning where we are scrolling too.
+
+// Positioning 
+
 moon.position.z = 30;
 moon.position.setX(-6);
-
-// moon2.position.z = -30;
-// moon2.position.setX(6);
 
 luke.position.z = -5;
 luke.position.x = 2;
 
 planetA.position.z = 5;
-planetA.position.y = 5;
+planetA.position.y = 0;
 planetA.position.x = 50;
 
+planetB.position.z = -60;
+planetB.position.y = 75;
+planetB.position.x = -70;
+
+planetC.position.z = -70;
+planetC.position.y = 30;
+planetC.position.x = 70;
+
 // Scroll animation
+
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
   moon.rotation.x += 0.05;
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
 
-  // moon2.rotation.x += 0.05;
-  // moon2.rotation.y += 0.075;
-  // moon2.rotation.z += 0.05;
-
   luke.rotation.y += 0.01;
   luke.rotation.z += 0.01;
 
   planetA.rotation.y += 0.01;
   planetA.rotation.z += 0.01;
+
+  // planetB.rotation.x += 0.05;
+  // planetB.rotation.y += 0.075;
+  // planetB.rotation.z += 0.05;
+
+  // planetC.rotation.x += 0.05;
+  // planetC.rotation.y += 0.075;
+  // planetC.rotation.z += 0.05;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
@@ -150,6 +185,7 @@ document.body.onscroll = moveCamera
 moveCamera();
 
 // Loop
+
 function animate(){
   requestAnimationFrame( animate );
 
@@ -165,11 +201,16 @@ function animate(){
   planetA.rotation.y += 0.005;
   planetA.rotation.z += 0.01;
 
+  // planetB.rotation.x += 0.001;
+  planetB.rotation.y += 0.001;
+  // planetB.rotation.z += 0.001;
+
+  // planetB.rotation.x += 0.001;
+  planetC.rotation.y -= 0.003;
+  // planetB.rotation.z += 0.001;
+
   moon.rotation.x += 0.001;
   moon.rotation.y += 0.003;
-
-  // moon2.rotation.x += 0.001;
-  // moon2.rotation.y += 0.003;
 
   // controls.update();
 
